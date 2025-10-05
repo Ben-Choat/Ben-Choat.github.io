be2cb73094897a59d919421501621dd65c5ed35f
Ben Choat <ben.choat@gmail.com>
2025-10-05 00:17:53 -0600

    separating navbar/footer to central location

--- assets/supplement.js
+++ assets/supplement.js
@@ -1,26 +1,29 @@
-window.myNamespace = Object.assign({}, window.myNamespace, {  
-    mySubNamespace: {  
-        // Enables custom icons to be used as gauge markers
-        pointToLayer: function(feature, latlng, context){
-            const drop = L.icon({
-                iconUrl: '/assets/Pepper.png',
-                iconSize: [24, 44],  // Adjust the size as needed
-                iconAnchor: [12, 44]  // Adjust the anchor point as needed
-            });
-            return L.marker(latlng, {icon: drop});
-        },  
 
-        // When hovering over a gauge, shows information about it
-        stationHover: function(feature, layer, context) {
-            console.log("stationHover function called");
-            // Extract latitude and longitude
-            const lat = feature.geometry.coordinates[1]; // Latitude
-            const lng = feature.geometry.coordinates[0]; // Longitude
-            
-            // Bind the tooltip to display latitude and longitude
-            layer.bindTooltip(`Lat: ${lat}, Lng: ${lng}`);
-        },
-
-                   
-    }  
+window.addEventListener("DOMContentLoaded", () => {
+  loadComponent("navbar", "/components/navbar.html").then(() => {
+    const links = document.querySelectorAll("nav a");
+    links.forEach(link => {
+      if (link.href === window.location.href) {
+        link.classList.add("active");  // style this in CSS
+      }
+    });
+  });
+  loadComponent("footer", "/components/footer.html");
 });
+
+
+async function loadComponent(id, file) {
+  const element = document.getElementById(id);
+  if (element) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) throw new Error(`Failed to fetch ${file}`);
+      const html = await response.text();
+      element.innerHTML = html;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+
